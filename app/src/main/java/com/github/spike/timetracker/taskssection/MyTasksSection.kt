a9ad34ddb90eb741fe478fa08031e087db0fbd64
Stephan <stephan@branczyk.org>
2022-09-27 15:32:02 -0700

    added colors and icons to task list

--- app/src/main/java/com/github/spike/timetracker/taskssection/MyTasksSection.kt
+++ app/src/main/java/com/github/spike/timetracker/taskssection/MyTasksSection.kt
@@ -13,15 +13,32 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.spike.timetracker.R
+import com.github.spike.timetracker.ui.theme.TimeTrackerTheme
 
 
 @Composable
 fun MyTasksSection() {
+    val tasks = arrayOf(
+        arrayOf("To Do", "5 tasks"),
+        arrayOf("In Progress",  "3 tasks"),
+        arrayOf("Done",  "25 tasks"),
+    )
+    val color_chips = arrayOf(
+        0xFFFDC698,
+        0xFFE0F6FD,
+        0xFFF3FD7F
+    )
+    val chip_icons = arrayOf(
+        R.drawable.ic_clipboard,
+        R.drawable.ic_pending_clipboard,
+        R.drawable.ic_check_mark_within_circle
+    )
     LazyColumn (modifier = Modifier
         .padding(top = 20.dp)
         .fillMaxWidth(),
@@ -30,7 +47,7 @@ fun MyTasksSection() {
         item {
             Text(
                 text = "My Tasks",
-                style = MaterialTheme.typography.h4,
+                style = MaterialTheme.typography.h5,
                 modifier = Modifier.padding(
                     start = 30.dp,
                     top = 8.dp,
@@ -49,19 +66,19 @@ fun MyTasksSection() {
             ) {
                 Card(
                     modifier = Modifier
-                        .size(80.dp)
+                        .size(40.dp)
                         .padding(start = 8.dp),
                     shape = RectangleShape,
                     elevation = 2.dp,
-                    backgroundColor = Color.Gray
+                    backgroundColor = Color(color_chips[index])
                 ) {
                Image(
-                   painterResource(id = R.drawable.ic_clipboard),
+                   painterResource(id = chip_icons[index]),
                    contentDescription = ""
                )
                 }
 
-                Text(text = "To Do",
+                Text(text = tasks[index][0],
                     style = MaterialTheme.typography.h6,
                     modifier = Modifier.padding(start=16.dp)
                 )
@@ -70,10 +87,10 @@ fun MyTasksSection() {
                         .width(IntrinsicSize.Min)
                         .weight(.1f)
                 )
-                Text(text = "5 tasks",
+                Text(text = tasks[index][1],
                     fontFamily = FontFamily.Default,
                     style = MaterialTheme.typography.h6,
-                    modifier = Modifier.size(100.dp)
+                    modifier = Modifier.width(100.dp)
                 )
 
 
@@ -82,8 +99,8 @@ fun MyTasksSection() {
                 modifier = Modifier.padding(
                     start = 28.dp,
                     end = 32.dp,
-                    top = 16.dp,
-                    bottom = 16.dp
+                    top = 8.dp,
+                    bottom = 8.dp
                 )
             )
         }
--- app/src/main/java/com/github/spike/timetracker/ui/theme/Color.kt
+++ app/src/main/java/com/github/spike/timetracker/ui/theme/Color.kt
@@ -6,3 +6,11 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val beige_pink = Color(0xFFFDC698)
+val beige_pink_lighter = Color(0xFFFEEDDE)
+val light_blue = Color(0xFFE0F6FD)
+val deep_black = Color(0xFF000000)
+val yello_green = Color(0xFFF3FD7F)
+
+
+
