d76a1ebcbe9786dbbe453acd97878f1d2a104b9b
Stephan <stephan@branczyk.org>
2022-09-27 13:50:04 -0700

    Initial commit

---
+++ app/src/main/java/com/github/spike/timetracker/taskssection/MyTasksSection.kt
@@ -0,0 +1,121 @@
+package com.github.spike.timetracker.taskssection
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun MyTasksSection() {
+    LazyColumn (modifier = Modifier
+        .padding(top = 20.dp)
+        .fillMaxWidth(),
+        contentPadding= PaddingValues(bottom = 40.dp)
+    ) {
+        item {
+            Text(
+                text = "My Tasks",
+                style = MaterialTheme.typography.h4,
+                modifier = Modifier.padding(
+                    start = 30.dp,
+                    top = 8.dp,
+                    bottom = 20.dp),
+            )
+        }
+        items(3) { index ->
+            Row(
+                modifier = Modifier
+                    .padding(
+                        start = 16.dp,
+                    )
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+
+            ) {
+                Card(
+                    modifier = Modifier
+                        .height(80.dp)
+                        .padding(start = 8.dp),
+                    shape = RectangleShape,
+                    elevation = 0.dp
+                ) {
+               //     Image(
+                 //       painterResource(covers[index].first),
+                   //     contentDescription = "",
+                   // )
+                }
+                Column(modifier = Modifier
+                    .padding(
+                        start = 16.dp,
+                        top = 4.dp,
+                        bottom = 4.dp
+                    )
+                    .weight(3f),
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text(text = "To Do",
+                        style = MaterialTheme.typography.h6
+                    )
+                }
+                Spacer(
+                    modifier = Modifier
+                        .width(IntrinsicSize.Max)
+                        .weight(.5f)
+                )
+                Column(modifier = Modifier
+                    .padding(
+                        top = 4.dp,
+                        bottom = 4.dp,
+                        end = 32.dp
+                    )
+                    .weight(1f),
+                    horizontalAlignment = Alignment.End
+                ) {
+                    Text(text = "5 tasks",
+                        fontFamily = FontFamily.Default,
+                        style = MaterialTheme.typography.h6
+                    )
+                    Spacer(
+                        modifier = Modifier
+                            .padding(top = 20.dp)
+                    )
+                    Card(
+                        modifier = Modifier.size(24.dp),
+                        shape = CircleShape,
+                        elevation = 0.dp
+                    ) {
+                       // DrawPercentageIndicator(percentages[index])
+                    }
+                }
+            }
+            Divider(
+                modifier = Modifier.padding(
+                    start = 28.dp,
+                    end = 32.dp,
+                    top = 16.dp,
+                    bottom = 16.dp
+                )
+            )
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreviewTasks() {
+    MyTasksSection()
+}
